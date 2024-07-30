554c4845c39438952796535767d6fa38012c5e66
Yash-Prajapati7 <157267869+Yash-Prajapati7@users.noreply.github.com>
2024-07-30 19:26:04 +0530

    added Word.js

---
+++ Word.js
@@ -0,0 +1,29 @@
+// This is a sample Word.js file. You can add more words if you want. The Word.js file I created for this algorithm contains more than 7000 words. 
+// However, I cannot post them all here, as it would compromise the secrecy of the passwords.
+
+const specialChars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
+const nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+const twoLetterWords = ['an', 'be', 'do', 'it', 'me'];
+const twoLetterWordsCaps = ['An', 'Be', 'Do', 'It', 'Me'];
+
+const threeLetterWords = ['cat', 'dog', 'hat', 'pen', 'sun'];
+const threeLetterWordsCaps = ['Cat', 'Dog', 'Hat', 'Pen', 'Sun'];
+
+const fourLetterWords = ['book', 'fish', 'game', 'love', 'moon'];
+const fourLetterWordsCaps = ['Book', 'Fish', 'Game', 'Love', 'Moon'];
+
+const fiveLetterWords = ['apple', 'grape', 'house', 'ocean', 'plane'];
+const fiveLetterWordsCaps = ['Apple', 'Grape', 'House', 'Ocean', 'Plane'];
+
+const sixLetterWords = ['banana', 'cherry', 'forest', 'island', 'planet'];
+const sixLetterWordsCaps = ['Banana', 'Cherry', 'Forest', 'Island', 'Planet'];
+
+const sevenLetterWords = ['avocado', 'broccoli', 'diamond', 'picture', 'sunrise'];
+const sevenLetterWordsCaps = ['Avocado', 'Broccoli', 'Diamond', 'Picture', 'Sunrise'];
+
+const eightLetterWords = ['elephant', 'hospital', 'keyboard', 'mountain', 'umbrella'];
+const eightLetterWordsCaps = ['Elephant', 'Hospital', 'Keyboard', 'Mountain', 'Umbrella'];
+
+const nineLetterWords = ['adventure', 'brilliant', 'challenge', 'difficult', 'fantastic'];
+const nineLetterWordsCaps = ['Adventure', 'Brilliant', 'Challenge', 'Difficult', 'Fantastic'];
